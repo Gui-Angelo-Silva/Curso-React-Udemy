7cd766b9b22371c5f7c89997cab92ad1bcc96c06
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-06-23 11:42:03 -0300

    Aula 47 - Componente Display

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -1,11 +1,14 @@
 import Reac, { Component } from 'react'
 import './Calculator.css'
+
 import Button from '../components/Button'
+import Display from '../components/Display'
 
 export default class Calculator extends Component {
     render(){
         return (
             <div className='calculator'>
+                <Display value={100}/>
                 <Button label="AC"/>
                 <Button label="/"/>
                 <Button label="7"/>
