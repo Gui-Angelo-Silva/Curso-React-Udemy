02bb3e8cd66553e1820047c0a59c654fc6c86022
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-06-23 09:34:43 -0300

    Aula 41 - Megasenha

--- fundamentos-react/src/components/mega/Mega.jsx
+++ fundamentos-react/src/components/mega/Mega.jsx
@@ -1,2 +1,23 @@
-import React from 'react'
+function gerarNumeros(qtde) {
+    const numeros = Array(qtde)
+        .fill(0)
+        .reduce((nums) => {
+            const NovoNumero = gerarNumeroNaoContido(1, 60, nums)
+            return [ ...nums, NovoNumero ]
+        }, [])
+        .sort((n1, n2) => n1 - n2)
 
+    return numeros
+}
+
+//console.log(gerarNumeros(7))
+
+function gerarNumeroNaoContido(min, max, array){
+    const aleatorio = parseInt(Math.random() * (max + 1 - min)) + min
+
+    return array.includes(aleatorio) ? 
+        gerarNumeroNaoContido(min, max, array) :
+        aleatorio
+}
+
+//console.log(gerarNumeroNaoContido(1, 60, [0, 0, 0, 0, 0, 0]))
