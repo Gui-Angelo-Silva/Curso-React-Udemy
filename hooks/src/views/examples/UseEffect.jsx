451c99c8062a0927917a3dc43839484839422700
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-06-28 12:01:07 -0300

    Aula não terminada

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 const UseEffect = (props) => {
+    const [number, setNumber] = useState(1)
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <div className="center">
+                <div>
+                    <span className="text">Fatorial:</span>
+                    <span className="text red">100</span>
+                </div>
+                <input type="number" className="text"
+                    value={number}
+                    onChange={e => setNumber(e.target.value)} />
+            </div>
         </div>
     )
 }
