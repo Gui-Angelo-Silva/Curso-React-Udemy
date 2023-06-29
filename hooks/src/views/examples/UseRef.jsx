d071afc0247208ca063c3d7b2f585e2d5c4a19c8
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-06-29 10:50:40 -0300

    feat: Added Hook UseRef with part two of the exercise, lesson 69

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -4,11 +4,21 @@ import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseRef = (props) => {
     const [value1, setValue1] = useState('')
+    const [value2, setValue2] = useState('')
+
     const count = useRef(0)
+    const myInput1 = useRef(null)
+    const myInput2 = useRef(null)
 
     useEffect(function() {
         count.current = count.current + 1
+        myInput2.current.focus()
     }, [value1])
+
+    useEffect(function() {
+        count.current++
+        myInput1.current.focus()
+    }, [value2])
 
     return (
         <div className="UseRef">
@@ -25,7 +35,15 @@ const UseRef = (props) => {
                     <span className="text">]</span>
                 </div>
                     <input type="text" className="input"
+                        ref={ myInput1 }
                         value={ value1 } onChange={ e => setValue1(e.target.value) } />
+            </div>
+
+            <SectionTitle title='Exercicio #02'></SectionTitle>
+            <div className="center">
+                <input type="text" className="input" 
+                    ref={ myInput2 }
+                    value={ value2 } onChange={ e => setValue2(e.target.value) }/>
             </div>
         </div>
     )
