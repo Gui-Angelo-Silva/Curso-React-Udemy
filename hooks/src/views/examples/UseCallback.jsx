fdb7459ceb50ea2b93cfd795ce2a7bd72aa251a7
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-06-30 09:00:58 -0300

    feat: Added the useCallback Hook that returns a memorized function, lesson 72

--- hooks/src/views/examples/UseCallback.jsx
+++ hooks/src/views/examples/UseCallback.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import UseCallbackButtons from './UseCallbackButtons'
 
-const UseCallback = (props) => {
+const UseCallback = props => {
+    const [count, setCount] = useState(0)
+
+    const inc = useCallback(function (delta) {
+        setCount(curr => curr + delta)
+    }, [setCount])
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
                 subtitle="Retorna uma função memoizada!"
             />
+            <div className="center">
+                <span className="text">{ count }</span>
+                <UseCallbackButtons inc={ inc }/>
+            </div>
         </div>
     )
 }
