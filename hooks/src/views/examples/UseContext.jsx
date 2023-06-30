a6df94474755ecb9e0e06afa0fa40b15b4f98623
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-06-30 11:24:23 -0300

    feat: Added the useContext Hook that returns the current value of the context, lesson 74, #01

--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+import DataContext from '../../data/DataContext'
+
 const UseContext = (props) => {
+
+    const {state, setState} = useContext(DataContext)
+    
+    function addNumber(delta) {
+        setState({
+            ...state,
+            number: state.number + delta
+        })
+    }
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+            <div className="center">
+                <span className="text">
+                    { state.text }
+                </span>
+                <span className="text">
+                    { state.number }
+                </span>
+
+                <div>
+                    <button className="btn"
+                        onClick={() => addNumber(-1)}>-1</button>
+                    <button className="btn"
+                        onClick={() => addNumber(1)}>+1</button>
+                </div>
+            </div>
         </div>
     )
 }
