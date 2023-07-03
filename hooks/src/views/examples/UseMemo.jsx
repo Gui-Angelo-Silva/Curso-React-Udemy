1ce47418fbf564ca1626841725abece6359a24c2
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-03 11:39:23 -0300

    feat: Added the Hook UseReducer, which obtains other states in functional components, lesson 76 - useReducer #01

--- hooks/src/views/examples/UseMemo.jsx
+++ hooks/src/views/examples/UseMemo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react'
+import React, { useState, useMemo } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 function sum(a, b){
--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+const initialState = {
+    cart: [],
+    products: [],
+    user: null,
+    //foco no número
+    number: 0
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'numberAdd2':
+            return { ...state, number: state.number + 2 }
+        case 'login':
+            return { ...state, user: { name: action.payload } }
+        default:
+            return state
+    }
+}
+
 const UseReducer = (props) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
     return (
         <div className="UseReducer">
             <PageTitle
                 title="Hook UseReducer"
                 subtitle="Uma outra forma de ter estado em componentes funcionais!"
             />
+
+            <div className="center">
+                {state.user ? (
+                    <span className="text">{state.user.name}</span>
+                ) : (
+                    <span className="text">Sem Usuário</span>
+                )}
+
+                <span className="text">{state.number}</span>
+                <div>
+                    <button className="btn"
+                        onClick={() => dispatch({ type: 'login', payload: 'Maria' })}>Login
+                    </button>
+                    <button className="btn"
+                        onClick={() => dispatch({ type: 'numberAdd2' })}>+2
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
