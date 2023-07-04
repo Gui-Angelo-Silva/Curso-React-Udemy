119973a3656096c7ba178dec0de590b2c6cd1fff
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-04 11:48:58 -0300

    feat: Adding an example of the UsoCustom Hook in the form of a counter, lesson 80, #01

--- hooks/src/views/examples/UseCustom.jsx
+++ hooks/src/views/examples/UseCustom.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+import { useCounter } from '../../hooks/useCounter'
 
 const UseRef = (props) => {
+
+    const [count, inc, dec] = useCounter(10)
+
     return (
         <div className="UseCustom">
             <PageTitle
                 title="Seu Hook"
                 subtitle="Vamos aprender como criar o nosso próprio Hook!"
             />
+            <SectionTitle title='Exercício #01'/>
+            <div className="center">
+                <span className="text">
+                    { count }
+                </span>
+                <div>
+                    <button className="btn"
+                        onClick={() => dec()}>-1</button>
+                    <button className="btn"
+                        onClick={() => inc()}>+1</button>
+                </div>
+            </div>
         </div>
     )
 }
